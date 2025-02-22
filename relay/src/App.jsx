411ab2ca3fb3dd49d5822666d70354cee268a675
Z Alos <satya123m4@gmail.com>
2025-02-23 00:23:49 +0530

    Basic WebSocket::Chat App

---
+++ relay/src/App.jsx
@@ -0,0 +1,71 @@
+import { useEffect, useRef, useState } from "react";
+import './App.css';
+
+const socket = new WebSocket('ws://localhost:8080', 'echo-protocol');
+
+function App() {
+    const msgInput = useRef(null);
+    const roomId = useRef(null);
+    const [messages, setMessage] = useState([
+        {type: "msg-in", message: "How Are You Bud!!!"},
+        {type: "msg-out", message: "Genius as Ever..."},
+        {type: "msg-out", message: "let's take bio-modification to the next level..."}
+    ]);
+
+    useEffect(() => {
+        socket.addEventListener('open', () => {
+            socket.send(JSON.stringify({type: "notify", message: "New User Joined..."}));
+        })
+        joinRoom();
+        msgInput.current.focus();
+        socket.addEventListener('message', (event) => {
+            const data = JSON.parse(event.data)
+            console.log("Message: ", data.message); 
+            setMessage(prevMessages => [
+                ...prevMessages,
+                {type: "msg-in", message: data.message}
+            ])
+        })
+    }, [])
+    
+    function joinRoom(){
+        roomId.current = window.prompt("Enter Room: ");
+        if(!roomId) joinRoom();
+        socket.send(JSON.stringify({type: "joinRoom", roomId: roomId.current})) 
+    }
+
+    const sendMsg = (e) => {
+        e.preventDefault();
+        const message = msgInput.current.value;
+        if(!message || message == "") return;
+        socket.send(JSON.stringify({type: "message", message: message, roomId: roomId.current}));
+        setMessage(prevMessages => [
+            ...prevMessages,
+            {type: "msg-out", message: message}
+        ])
+        msgInput.current.value = "";
+    }
+    
+  return (
+   <>
+   <div id="message-container">
+        {
+        messages.map((msg, id)=> {
+            if(msg.type == "msg-in"){
+            return <div key={id}><span className="message-in">{msg.message}</span></div>
+            }
+            else if (msg.type == "msg-out"){
+            return <div key={id}><span className="message-out">{msg.message}</span></div>
+            }
+        })
+        }
+    <form id="input-bar">
+        <input ref={msgInput} type="text" id="msg"/>
+        <button type="submit" onClick={sendMsg}>Send</button>
+    </form>
+    </div>
+   </> 
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,47 @@
+const WebSocketServer = require('websocket').server;
+const http = require('http');
+
+const server = http.createServer();
+
+const room = new Map; 
+server.listen(8080, () => {
+    console.log("server started on port 8080");
+})
+
+wsServer = new WebSocketServer({
+    httpServer: server,
+    // !todo() implement authenticity (origin)
+})
+
+wsServer.on('request', (request) => {
+    //accept connection over echo protocol
+    const connection = request.accept('echo-protocol');
+    connection.on('message', (data) => {
+        let message = JSON.parse(data.utf8Data);
+        console.log(message)
+        if(message.type == "joinRoom"){
+            const connections = room.get(message.roomId) || [];
+            room.set(message.roomId, [...connections, connection]);
+            // console.log(room.get(message.roomId))
+        }
+        if(message.type == "notify"){
+            console.log(message.message)
+        }
+        if(message.type == "message"){
+            console.log("Client: " + message.message);
+            const users = room.get(message.roomId);
+            console.log(users.length)
+            for(const user of users){
+                if(user.connected){
+                    if(user !== connection){
+                        user.sendUTF(JSON.stringify({type: "message", message: message.message}));
+                    }
+                }
+            }
+        }
+    })
+})
+
+
+
+
